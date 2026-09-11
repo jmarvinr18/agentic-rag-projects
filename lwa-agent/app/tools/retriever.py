5581f6b3c6686c2535547a00e9a0880d5710787c
marvin <jmarvin.ramoda@gmail.com>
2026-09-11 12:14:12 +0800

    added update with retriever file

--- lwa-agent/app/tools/retriever.py
+++ lwa-agent/app/tools/retriever.py
@@ -2,6 +2,7 @@ from langchain_core.tools.retriever import create_retriever_tool
 from app.embeddings.aws.s3vector import get_retriever
 # from app.embeddings.local.faiss import get_retriever
 
+
 def retriever_tool():
     """
     Retrieve from the vector store.
@@ -9,7 +10,7 @@ def retriever_tool():
     """
 
     retriever_tool_langchain = create_retriever_tool(
-        aws_retriever(),
+        get_retriever(),
         "retriever_vector_langchain_blog",
         "Search and run information about Langchain"
     )
@@ -19,31 +20,31 @@ def retriever_tool():
     return retriever_tool_langchain
 
 
-def aws_retriever():
+# def aws_retriever():
 
-    bedrock_config = Config(
-        connect_timeout=120,
-        read_timeout=120,
-        retries={"max_attempts": 3}
-    )
-    # bedrock-agent-runtime is what the retriever uses to call your KB
-    bedrock_agent_client = boto3.client(
-        "bedrock-agent-runtime",
-        region_name="ap-southeast-1",
-        config=bedrock_config
-    )
-    # --- Retriever pointing directly at your Bedrock Knowledge Base ---
-    aws_bedrock_retriever = AmazonKnowledgeBasesRetriever(
-        knowledge_base_id="DTDQDQUF9E",        # from AWS Bedrock console
-        client=bedrock_agent_client,           # explicit client
-        retrieval_config={
-            "vectorSearchConfiguration": {
-                "numberOfResults": 5,
-                "overrideSearchType": "SEMANTIC"   # or HYBRID
-            }
-        },
-    )
+#     bedrock_config = Config(
+#         connect_timeout=120,
+#         read_timeout=120,
+#         retries={"max_attempts": 3}
+#     )
+#     # bedrock-agent-runtime is what the retriever uses to call your KB
+#     bedrock_agent_client = boto3.client(
+#         "bedrock-agent-runtime",
+#         region_name="ap-southeast-1",
+#         config=bedrock_config
+#     )
+#     # --- Retriever pointing directly at your Bedrock Knowledge Base ---
+#     aws_bedrock_retriever = AmazonKnowledgeBasesRetriever(
+#         knowledge_base_id="DTDQDQUF9E",        # from AWS Bedrock console
+#         client=bedrock_agent_client,           # explicit client
+#         retrieval_config={
+#             "vectorSearchConfiguration": {
+#                 "numberOfResults": 5,
+#                 "overrideSearchType": "SEMANTIC"   # or HYBRID
+#             }
+#         },
+#     )
 
-    print(f"AWS_RETRIEVER: {aws_bedrock_retriever}")
+#     print(f"AWS_RETRIEVER: {aws_bedrock_retriever}")
 
-    return aws_bedrock_retriever
+#     return aws_bedrock_retriever
